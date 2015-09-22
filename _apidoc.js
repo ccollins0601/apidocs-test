d2623237b27a28bbdacb66c78f5b4afbf79a6423
Cal Collins <ccollins@cal-lenovo-t510>
2015-09-22 17:01:27 -0400

    experimenting with multiple versions

--- _apidoc.js
+++ _apidoc.js
@@ -10,19 +10,7 @@
 // ------------------------------------------------------------------------------------------
 // Current Errors.
 // ------------------------------------------------------------------------------------------
-/**
- * @apiDefine CreateUserError
- * @apiVersion 0.2.0
- *
- * @apiError NoAccessRight Only authenticated Admins can access the data.
- * @apiError UserNameTooShort Minimum of 5 characters required.
- *
- * @apiErrorExample  Response (example):
- *     HTTP/1.1 400 Bad Request
- *     {
- *       "error": "UserNameTooShort"
- *     }
- */
+
 
 
 // ------------------------------------------------------------------------------------------
@@ -30,71 +18,167 @@
 // ------------------------------------------------------------------------------------------
 /**
  * @apiDefinePermission admin Admin access rights needed.
- * Optionally you can write here further Informations about the permission.
  *
- * An "apiDefinePermission"-block can have an "apiVersion", so you can attach the block to a specific version.
- *
- * @apiVersion 0.3.0
+ * @apiVersion 3.7.0
  */
-
+/**
+ * @apiDefinePermission user Study or Site user role needed.
+ * See https://docs.openclinica.com for more information on user roles
+ *
+ * @apiVersion 3.7.0
+ */
 
 // ------------------------------------------------------------------------------------------
 // History.
 // ------------------------------------------------------------------------------------------
 /**
- * @apiDefinePermission admin This title is visible in version 0.1.0 and 0.2.0
- * @apiVersion 0.1.0
+ * @apiDefinePermission admin This title is visible in version 3.6.0
+ * @apiVersion 3.6.0
  */
 
 /**
- * @api {get} /user/:id Read data of a User
- * @apiVersion 0.2.0
- * @apiName GetUser
- * @apiGroup User
- * @apiPermission admin
+ * @api {get} /rest/metadata/:html/print/:study/:event/:form Return printable blank case report forms
+ * @apiVersion 3.6.0
+ * @apiName GetPrintableCRF
+ * @apiGroup Metadata
+ * @apiPermission user
  *
- * @apiDescription Here you can describe the function.
- * Multilines are possible.
+ * @apiDescription Annotated case report forms in printable HTML format. Use asterisks in place of OIDs as wildcards
  *
- * @apiParam {String} id The Users-ID.
+ * @apiParam {String} study Study or Site OID. Use '*' for all.
+ * @apiParam {String} event Study Event Definition OID. Use '*' for all.
+ * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
  *
- * @apiSuccess {String} id         The Users-ID.
- * @apiSuccess {Date}   name       Fullname of the User.
+ * @apiExample Example usage - gets all forms in a study:
+ * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/metadata/html/print/S_NCT02438/SE_ABC/F_123
  *
- * @apiError UserNotFound   The <code>id</code> of the User was not found.
+ * @apiSuccess {String}   ODM            The form(s) in HTML.
+ *
+ * @apiSuccessExample {html} Example success (html):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "forms displayed here"
+ *     }
+ *
+ * @apiError NoAccessRight Only authenticated users can access the data.
+ * @apiError NotFound   The resource was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
+/**
+ * @api {get} /rest/metadata/xml/view/:study/:event/:form Return CDISC ODM XML case report form definitions
+ * @apiVersion 3.6.0
+ * @apiName GetODM
+ * @apiGroup Metadata
+ * @apiPermission user
+ *
+ * @apiDescription Return CDISC ODM XML study definition. Use asterisks in place of OIDs as wildcards
+ *
+ * @apiParam {String} study Study or Site OID. Use '*' for all.
+ * @apiParam {String} event Study Event Definition OID. Use '*' for all.
+ * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
+ *
+ * @apiExample Example usage - gets all forms in a study:
+ * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/metadata/xml/view/S_NCT02438/SE_ABC/F_123
+ *
+ * @apiSuccess {String}   ODM            The study metadata in ODM XML.
+ *
+ * @apiSuccessExample {xml} Example success (xml):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "study ODM XML metadata here"
+ *     }
+ *
+ * @apiError NoAccessRight Only authenticated users can access the data.
+ * @apiError NotFound   The resource was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
+/**
+ * @api {get} /rest/metadata/json/view/:study/:event/:form Return CDISC ODM JSON case report form definitions
+ * @apiVersion 3.6.0
+ * @apiName GetODMJSON
+ * @apiGroup Metadata
+ * @apiPermission user
+ *
+ * @apiDescription Return CDISC ODM JSON study definition. Use asterisks in place of OIDs as wildcards
+ *
+ * @apiParam {String} study Study or Site OID. Use '*' for all.
+ * @apiParam {String} event Study Event Definition OID. Use '*' for all.
+ * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
+ *
+ * @apiExample Example usage - gets all forms in a study:
+ * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/metadata/json/view/S_NCT02438/SE_ABC/F_123
+ *
+ * @apiSuccess {String}   ODM            The form(s) in ODM JSON.
+ *
+ * @apiSuccessExample {json} Example success (json):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "study ODM metadata here"
+ *     }
+ *
+ * @apiError NoAccessRight Only authenticated users can access the data.
+ * @apiError NotFound   The resource was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
+/**
+ * @api {get} /rest/clinicaldata/html/print/:study/:subject/:event/:form Return blank case report forms
+ * @apiVersion 3.6.0
+ * @apiName GetPrintableCRF
+ * @apiGroup ClinicalData
+ * @apiPermission user
+ *
+ * @apiDescription Annotated case report forms in printable HTML format. Use asterisks in place of OIDs as wildcards
+ *
+ * @apiParam {String} study Study or Site OID.
+ * @apiParam {String} subject Subject OID.
+ * @apiParam {String} event Study Event Definition OID. Use '*' for all.
+ * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
+ *
+ * @apiExample Example usage - gets all forms in a study:
+ * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/clinicaldata/html/print/S_NCT02438/SS_123/SE_ABC/F_123
+ *
+ * @apiSuccess {String}   ODM            The form(s) in ODM XML, JSON, or HTML.
+ *
+ * @apiSuccessExample {json} Example success (json):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "ODM JSON metadata and subject clinical data here"
+ *     }
+ *
+ * @apiSuccessExample {xml} Example success (xml):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "ODM XML metadata and subject clinical data here"
+ *     }
+ *
+ * @apiSuccessExample {html} Example success (html):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "subject casebook here"
+ *     }
+ *
+ * @apiError NoAccessRight Only authenticated users can access the data.
+ * @apiError NotFound   The resource was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
  */
 
-/**
- * @api {get} /user/:id Read data of a User
- * @apiVersion 0.1.0
- * @apiName GetUser
- * @apiGroup User
- * @apiPermission admin
- *
- * @apiDescription Here you can describe the function.
- * Multilines are possible.
- *
- * @apiParam {String} id The Users-ID.
- *
- * @apiSuccess {String} id         The Users-ID.
- * @apiSuccess {Date}   name       Fullname of the User.
- *
- * @apiError UserNotFound   The error description text in version 0.1.0.
- */
-
-/**
- * @api {post} /user Create a User
- * @apiVersion 0.2.0
- * @apiName PostUser
- * @apiGroup User
- * @apiPermission none
- *
- * @apiDescription In this case "apiUse" is defined and used.
- * Define blocks with params that will be used in several functions, so you dont have to rewrite them.
- *
- * @apiParam {String} name Name of the User.
- *
- * @apiSuccess {String} id         The Users-ID.
- *
- * @apiUse CreateUserError
- */

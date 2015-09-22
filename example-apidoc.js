0a45d56b6c52b26686834e1a6813a169572c83cd
Cal Collins <ccollins@cal-lenovo-t510>
2015-09-22 16:55:42 -0400

    added detail to metadata calls, added clinicaldata methods

--- example-apidoc.js
+++ example-apidoc.js
@@ -1,14 +1,12 @@
 /**
- * @api {get} /rest/metadata/:format/:mode/:study/:event/:form Returns empty printable case report forms
+ * @api {get} /rest/metadata/:html/print/:study/:event/:form Return printable blank case report forms
  * @apiVersion 3.7.0
- * @apiName GetCRF
- * @apiGroup Form
+ * @apiName GetPrintableCRF
+ * @apiGroup Metadata
  * @apiPermission user
  *
- * @apiDescription Annotated case report forms in XML, JSON, or printable HTML format. Use asterisks in place of OIDs as wildcards
+ * @apiDescription Annotated case report forms in printable HTML format. Use asterisks in place of OIDs as wildcards
  *
- * @apiParam {String="html","xml","json"} format="html" Requested format. May be 'html', 'xml', or 'json'.
- * @apiParam {String="print","view"} mode="print" Requested format. May be 'print' or 'view'. Set to print if format is html, otherwise view
  * @apiParam {String} study Study or Site OID. Use '*' for all.
  * @apiParam {String} event Study Event Definition OID. Use '*' for all.
  * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
@@ -16,30 +14,9 @@
  * @apiExample Example usage - gets all forms in a study:
  * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/metadata/html/print/S_NCT02438/SE_ABC/F_123
  *
- * @apiSuccess {String}   id            The Users-ID.
- * @apiSuccess {Date}     registered    Registration Date.
- * @apiSuccess {Date}     name          Fullname of the User.
- * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
- * @apiSuccess {Object}   profile       Profile data (example for an Object)
- * @apiSuccess {Number}   profile.age   Users age.
- * @apiSuccess {String}   profile.image Avatar-Image.
- * @apiSuccess {Object[]} options       List of Users options (Array of Objects).
- * @apiSuccess {String}   options.name  Option Name.
- * @apiSuccess {String}   options.value Option Value.
+ * @apiSuccess {String}   ODM            The form(s) in HTML.
  *
- * @apiSuccessExample {json} Response (json):
- *     HTTP/1.1 200 Success
- *     {
- *       "odm": "study ODM metadata here"
- *     }
- *
- * @apiSuccessExample {xml} Response (xml):
- *     HTTP/1.1 200 Success
- *     {
- *       "odm": "study ODM XML metadata here"
- *     }
- *
- * @apiSuccessExample {html} Response (html):
+ * @apiSuccessExample {html} Example success (html):
  *     HTTP/1.1 200 Success
  *     {
  *       "odm": "forms displayed here"
@@ -54,37 +31,116 @@
  *       "error": "NoAccessRight"
  *     }
  */
-function getUser() { return; }
-
 /**
- * @api {post} /user Create a new User
- * @apiVersion 0.3.0
- * @apiName PostUser
- * @apiGroup User
- * @apiPermission none
+ * @api {get} /rest/metadata/xml/view/:study/:event/:form Return CDISC ODM XML case report form definitions
+ * @apiVersion 3.7.0
+ * @apiName GetODM
+ * @apiGroup Metadata
+ * @apiPermission user
  *
- * @apiDescription In this case "apiUse" is defined and used.
- * Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ * @apiDescription Return CDISC ODM XML study definition. Use asterisks in place of OIDs as wildcards
  *
- * @apiParam {String} name Name of the User.
+ * @apiParam {String} study Study or Site OID. Use '*' for all.
+ * @apiParam {String} event Study Event Definition OID. Use '*' for all.
+ * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
  *
- * @apiSuccess {String} id         The new Users-ID.
+ * @apiExample Example usage - gets all forms in a study:
+ * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/metadata/xml/view/S_NCT02438/SE_ABC/F_123
  *
- * @apiUse CreateUserError
+ * @apiSuccess {String}   ODM            The study metadata in ODM XML.
+ *
+ * @apiSuccessExample {xml} Example success (xml):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "study ODM XML metadata here"
+ *     }
+ *
+ * @apiError NoAccessRight Only authenticated users can access the data.
+ * @apiError NotFound   The resource was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
  */
-function postUser() { return; }
-
 /**
- * @api {put} /user/:id Change a new User
- * @apiVersion 0.3.0
- * @apiName PutUser
- * @apiGroup User
- * @apiPermission none
+ * @api {get} /rest/metadata/json/view/:study/:event/:form Return CDISC ODM JSON case report form definitions
+ * @apiVersion 3.7.0
+ * @apiName GetODMJSON
+ * @apiGroup Metadata
+ * @apiPermission user
  *
- * @apiDescription This function has same errors like POST /user, but errors not defined again, they were included with "apiUse"
+ * @apiDescription Return CDISC ODM JSON study definition. Use asterisks in place of OIDs as wildcards
  *
- * @apiParam {String} name Name of the User.
+ * @apiParam {String} study Study or Site OID. Use '*' for all.
+ * @apiParam {String} event Study Event Definition OID. Use '*' for all.
+ * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
  *
- * @apiUse CreateUserError
+ * @apiExample Example usage - gets all forms in a study:
+ * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/metadata/json/view/S_NCT02438/SE_ABC/F_123
+ *
+ * @apiSuccess {String}   ODM            The form(s) in ODM JSON.
+ *
+ * @apiSuccessExample {json} Example success (json):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "study ODM metadata here"
+ *     }
+ *
+ * @apiError NoAccessRight Only authenticated users can access the data.
+ * @apiError NotFound   The resource was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
  */
-function putUser() { return; }
+/**
+ * @api {get} /rest/clinicaldata/html/print/:study/:subject/:event/:form Return blank case report forms
+ * @apiVersion 3.7.0
+ * @apiName GetPrintableCRF
+ * @apiGroup ClinicalData
+ * @apiPermission user
+ *
+ * @apiDescription Annotated case report forms in printable HTML format. Use asterisks in place of OIDs as wildcards
+ *
+ * @apiParam {String} study Study or Site OID.
+ * @apiParam {String} subject Subject Key or ID.
+ * @apiParam {String} event Study Event Definition OID. Use '*' for all.
+ * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
+ *
+ * @apiExample Example usage - gets all forms in a study:
+ * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/clinicaldata/html/print/S_NCT02438/SS_123/SE_ABC/F_123
+ *
+ * @apiSuccess {String}   ODM            The form(s) in ODM XML, JSON, or HTML.
+ *
+ * @apiSuccessExample {json} Example success (json):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "ODM JSON metadata and subject clinical data here"
+ *     }
+ *
+ * @apiSuccessExample {xml} Example success (xml):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "ODM XML metadata and subject clinical data here"
+ *     }
+ *
+ * @apiSuccessExample {html} Example success (html):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "subject casebook here"
+ *     }
+ *
+ * @apiError NoAccessRight Only authenticated users can access the data.
+ * @apiError NotFound   The resource was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
+

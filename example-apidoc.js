7fb8e79d88e3fe4f6c3bd7334582ae5d46fbc656
Cal Collins <ccollins@cal-lenovo-t510>
2015-09-22 16:32:49 -0400

    updated API with OpenClinica example

--- example-apidoc.js
+++ example-apidoc.js
@@ -1,16 +1,20 @@
 /**
- * @api {get} /user/:id Read data of a User
- * @apiVersion 0.3.0
- * @apiName GetUser
- * @apiGroup User
- * @apiPermission admin
+ * @api {get} /rest/metadata/:format/:mode/:study/:event/:form Returns empty printable case report forms
+ * @apiVersion 3.7
+ * @apiName GetCRF
+ * @apiGroup Form
+ * @apiPermission user
  *
- * @apiDescription Compare Verison 0.3.0 with 0.2.0 and you will see the green markers with new items in version 0.3.0 and red markers with removed items since 0.2.0.
+ * @apiDescription Annotated case report forms in XML, JSON, or printable HTML format. Use asterisks in place of OIDs as wildcards
  *
- * @apiParam {String} id The Users-ID.
+ * @apiParam {String="html","xml","json"} format="html" Requested format. May be 'html', 'xml', or 'json'.
+ * @apiParam {String="print","view"} mode="print" Requested format. May be 'print' or 'view'. Set to print if format is html, otherwise view
+ * @apiParam {String} study Study or Site OID. Use '*' for all.
+ * @apiParam {String} event Study Event Definition OID. Use '*' for all.
+ * @apiParam {String} form Case Report Form Version OID. Use '*' for all.
  *
- * @apiExample Example usage:
- * curl -i http://localhost/user/4711
+ * @apiExample Example usage - gets all forms in a study:
+ * curl -i demo2.eclinicalhosting.com/OpenClinica9/rest/metadata/html/print/S_NCT02438/*/*
  *
  * @apiSuccess {String}   id            The Users-ID.
  * @apiSuccess {Date}     registered    Registration Date.
@@ -23,8 +27,26 @@
  * @apiSuccess {String}   options.name  Option Name.
  * @apiSuccess {String}   options.value Option Value.
  *
- * @apiError NoAccessRight Only authenticated Admins can access the data.
- * @apiError UserNotFound   The <code>id</code> of the User was not found.
+ * @apiSuccessExample {json} Response (json):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "study ODM metadata here"
+ *     }
+ *
+ * @apiSuccessExample {xml} Response (xml):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "study ODM XML metadata here"
+ *     }
+ *
+ * @apiSuccessExample {html} Response (html):
+ *     HTTP/1.1 200 Success
+ *     {
+ *       "odm": "forms displayed here"
+ *     }
+ *
+ * @apiError NoAccessRight Only authenticated users can access the data.
+ * @apiError NotFound   The resource was not found.
  *
  * @apiErrorExample Response (example):
  *     HTTP/1.1 401 Not Authenticated
